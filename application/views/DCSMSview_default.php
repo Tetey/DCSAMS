29102857b1a7ead6e51095d8f78308a7daeeeddf
Tetey <arg_lagunda@yahoo.com.ph>
2015-02-24 00:39:47 -0800

    I added everything here. - Tetey

---
+++ application/views/DCSMSview_default.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
+	<link rel="stylesheet" href="<?php echo base_url("assets/css/styles.css"); ?>" />
+	<!--link rel="shortcut icon" href="img/dcs_logo.ico"-->
+	<title>DCS</title>
+</head>
+<body>
+<div class="container-full">
+
+      <div class="row">
+       
+        <div class="col-lg-12 text-center v-center">
+          
+          <h1>Hello Landing</h1>
+          <p class="lead">A sign-up page example for Bootstrap 3</p>
+          
+          <br><br><br>
+          
+          <form class="col-lg-12">
+            <div class="input-group" style="width:400px;text-align:center;margin:0 auto;">
+            <input class="form-control input-lg" title="Don't worry. We hate spam, and will not share your email with anyone." placeholder="Search" type="text">
+              <span class="input-group-btn">
+              <button type="button" class="btn btn-lg btn-success dropdown-toggle" data-toggle="dropdown">Student Number<span class = "caret"></button>
+   				<ul class="dropdown-menu" role="menu">
+		    	<li><a href="#">Student Number</a></li>
+		    	<li><a href="#">Last Name</a></li>
+		    	<li><a href="#"></a></li>
+   				</ul></span>
+            	
+            </div>
+            <br><button class="btn btn-lg btn-primary" type="button">SEARCH</button><p>        </p><button class="btn btn-lg btn-primary" type="button">SHOW ALL</button>
+          </form>
+        </div>
+        
+      </div> <!-- /row -->
+  
+  	<br><br><br><br><br>
+
+</div> <!-- /container full -->
+
+
+<script type="text/javascript" src="<?php echo base_url("assets/js/jquery-1.11.2.min.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+</body>
+</html>
